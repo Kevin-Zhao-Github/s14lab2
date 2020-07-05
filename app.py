c3b581764f1ce7b8aa9e3796c6f27936d08fd081
Kevin-Zhao-Github <Kevin_Zhao@student.wayland.k12.ma.us>
2020-07-04 20:28:08 -0400

    Bootstrap test

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello_world():
     model1 = joblib.load('lin_regr.pkl')
     pred1 = model1.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
     model2 = joblib.load('decision_tree.pkl')
-    pred2 = model2.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
+    pred2 = model2.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0].round(1)
     return render_template('index.html', preds=(pred1, pred2))
